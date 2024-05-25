02c08d4b1e9fa1e019e108cae4c01cb167be2d83
Kamilla <kamilla@MacBook-Air-Kamilla.local>
2024-05-25 22:04:10 +0300

    page not found

---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+import './PageNotFound.styles.css'
+
+const PageNotFound = () => {
+    return (
+        <div className="pgn-body">
+            <div className="img-pnf">
+                <img src="/public/icons/broken_link.svg" alt="404" />
+            </div>
+            <div className="text-pnf">Page not found</div>
+            <Link to="/" className="link-pnf">
+                    Return to home page
+            </Link>
+        </div>
+    )
+}
+
+export default PageNotFound
